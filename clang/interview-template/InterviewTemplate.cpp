389c41ecfab71f24cb2d5ec1cf79e3671f11c1df
Kyle Headley <kheadley@uab.edu>
2020-10-19 11:51:01 -0400

    Kyle's Interview solution

--- clang/interview-template/InterviewTemplate.cpp
+++ clang/interview-template/InterviewTemplate.cpp
@@ -9,6 +9,36 @@
 using namespace clang;
 using namespace clang::tooling;
 
+std::string LastFn;
+std::map<std::string,std::set<std::string>> FnGraph;
+
+void FindLoops(
+    // std::map<std::string,std::set<std::string>> *FnGraph,
+    std::vector<std::string> *Path,
+    std::set<std::string> *Found,
+    std::string Start) {
+  if (Found->count(Start)) {
+    return;
+  }
+  auto *Branches = &FnGraph[Start];
+  for (auto itr = Branches->begin(); itr != Branches->end(); ++itr) {
+    bool Loop = false;
+    for (unsigned long i = 0; i < Path->size(); ++i) {
+      if ( (*Path)[i] == *itr) {
+        Loop = true;
+      }
+      if (Loop) {
+        Found->insert((*Path)[i]);
+      }
+    }
+    if (!Loop) {
+      Path->push_back(*itr);
+      FindLoops(Path,Found,*itr);
+      Path->pop_back();
+    }
+  }
+}
+
 class InterviewASTVisitor
         : public RecursiveASTVisitor<InterviewASTVisitor> {
 public:
@@ -16,8 +46,18 @@ public:
             : Context(Context) {}
 
     bool VisitFunctionDecl(FunctionDecl *Decl) {
-        // TODO:
-        return true;
+      LastFn = Decl->getDeclName().getAsString();
+      std::set<std::string> empty;
+      FnGraph.insert({LastFn, empty});
+      // llvm::errs() << Decl->getDeclName().getAsString() << "\n";
+      return true;
+    }
+
+    bool VisitCallExpr(CallExpr *Call) {
+      std::string CallName = Call->getDirectCallee()->getDeclName().getAsString();
+      FnGraph[LastFn].insert(CallName);
+      // llvm::errs() << "call: " << Call->getDirectCallee()->getDeclName().getAsString() << "\n";
+      return true;
     }
 
 private:
@@ -31,6 +71,25 @@ public:
 
     virtual void HandleTranslationUnit(clang::ASTContext &Context) {
         Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+
+        std::vector<std::string> Path;
+        std::set<std::string> Found;
+        for (auto itr = FnGraph.begin(); itr != FnGraph.end(); ++itr) {
+          FindLoops(&Path,&Found,itr->first);
+        }
+
+        llvm::errs() << "Syntactic Recursive Functions: \n";
+        for (auto i = Found.begin(); i != Found.end(); ++i) {
+          llvm::errs() << *i << "\n";
+        }
+
+//        // Debug out
+//        for (auto itr = FnGraph.begin(); itr != FnGraph.end(); ++itr) {
+//          llvm::errs() << itr->first << "\n";
+//          for (auto i = itr->second.begin(); i != itr->second.end(); ++i) {
+//            llvm::errs() << "call: " << *i << "\n";
+//          }
+//        }
     }
 
 private:
